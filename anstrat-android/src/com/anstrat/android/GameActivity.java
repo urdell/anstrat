a23cc53aea6caa2141510cc26990796602fba1a8
EkisSuper <EkisSuper@gmail.com>
2012-06-14 13:52:38 +0000

    initial

---
+++ anstrat-android/src/com/anstrat/android/GameActivity.java
@@ -0,0 +1,18 @@
+package com.anstrat.android;
+
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+
+import com.anstrat.core.Main;
+import com.badlogic.gdx.backends.android.AndroidApplication;
+
+public class GameActivity extends AndroidApplication {
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        initialize(Main.getInstance(), false);
+        
+    }
+}
