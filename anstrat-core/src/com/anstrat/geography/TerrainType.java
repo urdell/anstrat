787ef54a41439509be2398097f751c2b6834e130
aabbqrstp <aabbqrstp@92ae5604-1559-cc0f-a502-5bd1a2f9eb42>
2012-07-10 06:23:30 +0000

    Terraintype now includes texture names as well. Changed GTile accordingly.

--- anstrat-core/src/com/anstrat/geography/TerrainType.java
+++ anstrat-core/src/com/anstrat/geography/TerrainType.java
@@ -1,22 +1,24 @@
 package com.anstrat.geography;
 
 public enum TerrainType {
-	DEEP_WATER("Deep Water", 1),
-	//SNOW("Snow", 1),
-	FIELD("Field", 1),
-	MOUNTAIN("Mountain", 1),
-	//VOLCANO("Volcano", 1),
-	FOREST("Forest", 1),
-	//HILL("Hill", 2),
-	SHALLOW_WATER("Shallow Water", 1),
-	CASTLE("Castle", 2),
-	VILLAGE("Village", 1);
+	DEEP_WATER("Deep Water", 1, "water-0001","water-0002"),
+	//SNOW("Snow", 1, "snow"),
+	FIELD("Field", 1, "grass"),
+	MOUNTAIN("Mountain", 1, "mountain"),
+	//VOLCANO("Volcano", 1, "volcano-0001","volcano-0002"),
+	FOREST("Forest", 1, "forest"),
+	//HILL("Hill", 2, "hill"),
+	SHALLOW_WATER("Shallow Water", 1, "shallow-0001","shallow-0002"),
+	CASTLE("Castle", 2, "castle"),
+	VILLAGE("Village", 1, "village");
 	
 	public transient final String name;
 	public transient final int defenceBonus;
+	public transient final String[] textures;
 	
-	private TerrainType(String name, int defenceBonus){
+	private TerrainType(String name, int defenceBonus, String ... textures){
 		this.name = name;
 		this.defenceBonus = defenceBonus;
+		this.textures = textures;
 	}
 }
