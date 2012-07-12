8f4c906854fba590179f16b293325758548aedd9
teerik@student.chalmers.se <teerik@student.chalmers.se@92ae5604-1559-cc0f-a502-5bd1a2f9eb42>
2012-07-12 14:24:02 +0000

    added proper handling of passwords on the server side

--- trunk/anstrat-server/src/com/anstrat/server/AuthMessageHandler.java
+++ trunk/anstrat-server/src/com/anstrat/server/AuthMessageHandler.java
@@ -1,14 +1,11 @@
 package com.anstrat.server;
 
 import java.io.Serializable;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.util.Locale;
 import java.util.Random;
 
 import com.anstrat.network.NetworkMessage;
 import com.anstrat.server.db.DatabaseHelper;
-import com.anstrat.server.db.DatabaseHelper.DatabaseType;
 
 /**
  * This class handles all NetworkMessages related to authentication.
@@ -41,63 +38,24 @@ public class AuthMessageHandler {
 	public synchronized void login(String username, String password, PlayerSocket socket)
 	{
 		NetworkMessage response;
+		
 		// Usernames should consistently be handled in all lower case on the server side.
 		String lowerCaseUsername = username.toLowerCase(Locale.ENGLISH);
 
-		/* If the socket already is logged in as the user in question, the server
-		 * should be robust 
-		 */
+		// Is user already logged in?
 		if(lowerCaseUsername.equals(socket.getUsername()))
 		{
 			response = new NetworkMessage("ACCEPT_LOGIN", (Serializable) socket.getNetworkName(), socket.getUser().getUserId());
 		}
 		else
 		{
-			java.sql.Connection conn = null;
-			PreparedStatement pst = null;
-			ResultSet rs = null;
+			User user = DatabaseHelper.getUser(lowerCaseUsername);
 			
-			try
-			{
-				conn = DatabaseHelper.connect(DatabaseType.PostgreSQL);
-			
-				pst = conn.prepareStatement("SELECT * FROM USERS WHERE USERNAME = ? AND PASSWORD = ?");
-				pst.setString(1, lowerCaseUsername);
-				pst.setString(2, HashUtil.getHash("SHA-256", password));
-				
-				rs = pst.executeQuery();
-				
-				if(rs.next())
-				{
-					PlayerSocket anotherInstance = server.getSocketForUser(lowerCaseUsername);
-					if(anotherInstance!=null)
-					{
-						server.logout(anotherInstance, true);
-						anotherInstance.sendMessage(new NetworkMessage("LOGIN_OVERRIDE","You have been logged out since you logged in elsewhere."));
-					}
-					
-					User user = new User(rs.getInt("id"), rs.getString("username"), rs.getString("displayedName"));
-					socket.setUser(user);
-					server.login(socket, user.getUsername(), user.getDisplayedName());
-					response = new NetworkMessage("ACCEPT_LOGIN", (Serializable) user.getDisplayedName(), user.getUserId());
-				}
-				else
-				{
-					server.logln(socket.getConnection().toString()+" failed to log in as "+lowerCaseUsername+".");
-					response = new NetworkMessage("DENY_LOGIN",(Serializable) "Invalid username or password. Please try again.");
-				}
+			if(user == null || !PasswordUtil.authenticate(password, user.getEncryptedPassword())){
+				response = new NetworkMessage("DENY_LOGIN",(Serializable) "Invalid username or password. Please try again.");
 			}
-			catch(Exception e)
-			{
-				e.printStackTrace();
-				
-				response = new NetworkMessage("DENY_LOGIN",(Serializable) "An unknown error occurred. Please try again.");
-			}
-			finally
-			{
-				DatabaseHelper.closeResSet(rs);
-				DatabaseHelper.closeStmt(pst);
-				DatabaseHelper.closeConn(conn);
+			else{
+				response = new NetworkMessage("ACCEPT_LOGIN", (Serializable) user.getDisplayedName(), user.getUserId());
 			}
 		}
 
@@ -127,18 +85,14 @@ public class AuthMessageHandler {
 				if(!DatabaseHelper.usernameTaken(name) && !DatabaseHelper.displayednameTaken(name))
 				{
 					String password = ((Long) rand.nextLong()).toString();
+					User user = DatabaseHelper.createUser(name, password, name);
 					
-					Long userId = DatabaseHelper.createUser(name, password, name);
+					if(user == null) break;
 					
-					if(userId==-1l)
-						break;
-					else
-					{
-						socket.sendMessage(new NetworkMessage("ACCEPT_QUICK",userId,name,password));
-						socket.setUser(new User(userId,name,name));
-						server.login(socket, name, name);
-						return;
-					}
+					socket.sendMessage(new NetworkMessage("ACCEPT_QUICK", user.getUserId(), user.getUsername(), password));
+					socket.setUser(user);
+					server.login(socket, name, name);
+					return;
 				}
 			}
 			
