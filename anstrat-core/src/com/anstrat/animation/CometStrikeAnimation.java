45fd21f28730657c417ba97a1f93756fcf9a8d25
EkisSuper <EkisSuper@gmail.com>
2012-07-17 18:15:53 +0000

    Is fixes

--- anstrat-core/src/com/anstrat/animation/CometStrikeAnimation.java
+++ anstrat-core/src/com/anstrat/animation/CometStrikeAnimation.java
@@ -18,13 +18,15 @@ import com.badlogic.gdx.math.Vector2;
 
 public class CometStrikeAnimation extends Animation {
 	private GTile target;
-	private boolean started = false;
+	private boolean started = false, started2 = false;
 	private float timePassed = 0;
-	private Vector2 start = null;
 	private Vector2 end = null;
-	
+	private float xdiff = 0;
+	private float ydiff = 0;
 	com.badlogic.gdx.graphics.g2d.Animation animation1 = null;
 	com.badlogic.gdx.graphics.g2d.Animation animation2 = null;
+	
+	Animation shake1 = null, shake2= null, move = null;
 	
 	public CometStrikeAnimation(TileCoordinate target) {
 		animation1 = Assets.getAnimation("meteorstrike");
@@ -33,14 +35,18 @@ public class CometStrikeAnimation extends Animation {
 		
 				
 		this.target = GEngine.getInstance().getMap().getTile(target);
-		length = animation1.animationDuration*7+animation2.animationDuration;
+		length = animation1.animationDuration*4+animation2.animationDuration;
 		lifetimeLeft = length;
 		
-		start = this.target.getPosition();
-		start.x += 3*GEngine.getInstance().map.TILE_WIDTH*3;
-		start.y += 3*GEngine.getInstance().map.TILE_HEIGHT*3;
 		
 		end = this.target.getPosition();
+		
+		xdiff = 3*GEngine.getInstance().map.TILE_WIDTH;
+		ydiff = 3*GEngine.getInstance().map.TILE_HEIGHT;
+		
+		shake1 = new ShakeCamAnimation(new Vector2(GEngine.getInstance().camera.position.x, GEngine.getInstance().camera.position.y), GEngine.getInstance().map.TILE_WIDTH/25, GEngine.getInstance().map.TILE_HEIGHT/25, length/6, 15);
+		shake2 = new ShakeCamAnimation(new Vector2(GEngine.getInstance().camera.position.x, GEngine.getInstance().camera.position.y), GEngine.getInstance().map.TILE_WIDTH/15, GEngine.getInstance().map.TILE_HEIGHT/15, 1.7f*animation2.animationDuration, 25);
+		
 	}
 	
 	@Override
@@ -48,7 +54,9 @@ public class CometStrikeAnimation extends Animation {
 		// Run once
 		if(!started){
 			started = true;	
+			GEngine.getInstance().animationHandler.runParalell(shake1);
 		}
+		
 		if(lifetimeLeft <= 0f){
 			Unit centerTarget = StateUtils.getUnitByTile(target.tile.coordinates);
 			if(centerTarget != null){
@@ -74,7 +82,10 @@ public class CometStrikeAnimation extends Animation {
 				}
 			}
 		}
-		
+		if (lifetimeLeft <= animation2.animationDuration && started2 == false) {
+			started2 = true;
+			GEngine.getInstance().animationHandler.runParalell(shake2);
+		}
 		timePassed += deltaTime;
 	}
 	
@@ -89,20 +100,21 @@ public class CometStrikeAnimation extends Animation {
 		
 		GMap map = GEngine.getInstance().map;
 		Vector2 position = new Vector2();
-		position.x = end.x-(end.x-start.x)*var; 
-		position.y = end.y+(end.y-start.y)*var;
+		position.x = end.x+xdiff*(float)Math.pow(var, 3); 
+		position.y = end.y-ydiff*(float)Math.pow(var, 3);
 		
 		float width = map.TILE_WIDTH;
 		float height = map.TILE_HEIGHT;
 		
-		
+		float cw = width*(0.75f+(10*(float)Math.pow(var, 4)));
+		float ch = height*(0.75f+(10*(float)Math.pow(var, 4)));
 		
 		if (lifetimeLeft > animation2.animationDuration){
 			TextureRegion region = this.animation1.getKeyFrame(timePassed, true);
-			batch.draw(region, position.x, position.y, width, height);
+			batch.draw(region, position.x-cw/2+width/2, position.y-ch/2+height/2, cw, ch);
 		}
 		else {
-			TextureRegion region = this.animation2.getKeyFrame(timePassed-animation1.animationDuration*7, false);
+			TextureRegion region = this.animation2.getKeyFrame(timePassed-animation1.animationDuration*4, false);
 			batch.draw(region, position.x-(width/2), position.y-(width/2), width*2, height*2);
 		
 		}
