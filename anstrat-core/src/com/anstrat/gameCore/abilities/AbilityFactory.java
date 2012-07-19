16efe78a80a88a1cb5a8794262d7389f2998d1cb
tompa1337 <tompa1337@gmail.com>
2012-07-19 12:01:19 +0000

    Adding LifeSteal

--- anstrat-core/src/com/anstrat/gameCore/abilities/AbilityFactory.java
+++ anstrat-core/src/com/anstrat/gameCore/abilities/AbilityFactory.java
@@ -5,11 +5,13 @@ public class AbilityFactory {
 	public static final int NOTHING = 0;
 	public static final int BERSERK = 1;
 	public static final int HEAL = 2;
+	public static final int LIFESTEAL = 3;
 
 	public static Ability createAbility(int abilityId){
 		switch(abilityId){
 			case BERSERK: return new Berserk();
 			case HEAL: return new Heal();
+			case LIFESTEAL: return new LifeSteal();
 			default: return null;
 		}
 	}
