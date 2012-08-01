2f68cc3f0f8c232b98fb429b4679c6c97a337bb9
teerik <teerik@student.chalmers.se>
2012-08-01 16:58:33 +0000

    refactored the list of games

--- anstrat-core/src/com/anstrat/menu/MainMenu.java
+++ anstrat-core/src/com/anstrat/menu/MainMenu.java
@@ -1,7 +1,5 @@
 package com.anstrat.menu;
 
-import java.util.Date;
-import java.util.HashMap;
 import java.util.Random;
 
 import com.anstrat.core.Assets;
@@ -162,35 +160,6 @@ public class MainMenu extends MenuScreen {
 		return me;
 	}
 	
-	private float timeSinceLastUpdate = 0f;
-	//private float flagAnimationStateTime = 0f;
-	
-	@Override
-	public void render(float delta){
-		super.render(delta);
-		
-		if(timeSinceLastUpdate > 0.5f){
-			updateTimeleftLabels();
-			
-			timeSinceLastUpdate = 0f;
-		}
-	}
-	
-	private void updateTimeleftLabels(){
-		// Update time labels
-		long now = new Date().getTime();
-		
-		for(java.util.Map.Entry<NetworkGameInstance, Label> e : timeleftLabels.entrySet()){
-			long diff = e.getKey().getTurnEndTime().getTime() - now; 
-			
-			if(diff < 0){
-				e.getValue().setText("TURN ENDED");
-			}
-			else{
-				e.getValue().setText(formatTime(diff));
-			}
-		}
-	}
 	
 	@Override
 	public void hide() {
@@ -209,13 +178,10 @@ public class MainMenu extends MenuScreen {
 		me = null;
 	}
 	
-	private java.util.Map<NetworkGameInstance, Label> timeleftLabels = new HashMap<NetworkGameInstance, Label>();
-	
 	public void updateGamesList(){
 		// Only update if MainMenu is the active screen
 		if(Main.getInstance().getScreen() != this) return;
 		
-		timeleftLabels.clear();
 		gamesList.clear();
 		gamesList.top();
 		gamesList.setFillParent(true);
@@ -223,173 +189,48 @@ public class MainMenu extends MenuScreen {
 		int height = (int)(Main.percentHeight*4);
 		int paddingTop = (int)(Main.percentHeight*4);
 		
+		// Game instances
 		Table current = new Table(Assets.SKIN);
 		current.add("Your turn:").height(height).padTop(paddingTop);
 		current.row();
 		
 		Table waiting = new Table(Assets.SKIN);
 		waiting.add("Waiting for other players:").height(height).padTop(paddingTop);
-		waiting.row();
 		
+        for(final GameInstance gi : GameInstance.getActiveGames()){
+        	
+        	// Check if it's the player's turn (it's always the player's turn for offline games)
+        	boolean isPlayerTurn = gi.getCurrentPlayer().userID == User.globalUserID || !(gi instanceof NetworkGameInstance);
+        	
+        	Table table = isPlayerTurn ? current : waiting;
+        	table.row();
+        	table.add(gameInstanceToTable(gi)).fillX().expandX().height((int)(17*Main.percentHeight));
+        }
+		
+        // Game requests
 		Table requests = new Table(Assets.SKIN);
 		requests.add("Game requests:").height(height).padTop(paddingTop);
-		requests.row();
 		
-		NinePatch gameTableBackground = Assets.SKIN.getPatch("line-border-thin");
-		
-    	//Add GameInstances
-        for(final GameInstance gi : GameInstance.getActiveGames()){
-        	if(!(gi instanceof NetworkGameInstance) || gi.isInGame(User.globalUserID))
-        	{
-            	boolean isPlayerTurn = gi.getCurrentPlayer().userID == User.globalUserID;
-            	Table t = new Table(Assets.SKIN);
-            	Label timeleftLabel = null;
-            	Label turn = new Label("Turn "+gi.getTurnNumber(),Assets.SKIN);
-            	Label type = new Label("("+(gi.isAiGame() ? "AI" : 
-            		(gi instanceof NetworkGameInstance ? "Network" : "Hotseat"))+")", Assets.SKIN);
-            	Label map = new Label("'"+gi.state.map.name+"'",Assets.SKIN);
-            	Label mapSize = new Label(gi.state.map.getXSize()+"x"+gi.state.map.getYSize(),Assets.SKIN);
-            	
-            	t.setBackground(gameTableBackground);
-            	
-            	if(gi instanceof NetworkGameInstance){
-            		timeleftLabels.put((NetworkGameInstance) gi, timeleftLabel);
-            	}
-            	
-            	Button cancel = new Button(new Image(Assets.getTextureRegion("cancel")), Assets.SKIN.getStyle("image-toggle", ButtonStyle.class));
-            	
-            	cancel.setClickListener(new ClickListener() {
-        	        @Override
-        	        public void click(Actor actor,float x,float y ){
-        	        	gi.resign();
-        	        	updateGamesList();
-        	        }
-        		});
-            	
-            	String opponent = "vs. ";
-            	if(gi.isAiGame() || gi instanceof NetworkGameInstance)
-    	        	for(Player p : gi.state.players){
-    	        		if(p.userID != User.globalUserID){
-    	        			opponent += p.displayedName;
-    	        			break;
-    	        		}
-    	        	}
-            	else{
-            		opponent = gi.state.players[0].displayedName + " vs. " +gi.state.players[1].displayedName;
-            	}
-            	Label players = new Label(opponent, Assets.SKIN);
-            	
-            	t.left().pad((int)(2*Main.percentWidth));
-            	t.defaults().left().fillX().expandX();
-            	
-            	Table outer = new Table();
-            	outer.defaults().left().height((int)(4*Main.percentHeight));
-            	outer.add(players);
-            	outer.row();
-            	
-            	Table inner1 = new Table();
-            	inner1.add(map).fillX().expandX();
-            	inner1.add(mapSize);
-            	
-            	Table inner2 = new Table();
-            	inner2.add(type);
-            	inner2.add(turn).expandX();
-            	inner2.add(timeleftLabel);
-            	
-            	outer.add(inner1).expandX().fillX();
-            	outer.row();
-            	outer.add(inner2).expandX().fillX();
-            	
-            	t.add(outer).expandX().fillX().padLeft((int)(Main.percentHeight));
-            	t.add(cancel).pad((int)(3+Main.percentWidth)).height((int)(7*Main.percentHeight)).width((int)(7*Main.percentHeight)).bottom().right();
-            	
-            	Table tab = (isPlayerTurn || !(gi instanceof NetworkGameInstance))?current:waiting;
-            	tab.add(t).fillX().expandX().height((int)(17*Main.percentHeight));
-            	tab.row();
-            	t.setClickListener(new ClickListener() {
-        	        @Override
-        	        public void click(Actor actor,float x,float y ){
-        	        	gi.showGame(false);
-        	        }
-        		});
-            }
-        }
-       
-        //Add game requests
-        for(GameRequest gr : Main.getInstance().gameRequests.values()){
-        	
-        	Table t = new Table(Assets.SKIN);
-        	t.setBackground(gameTableBackground);
-        	
-        	Map map = gr.map;
-        	Label gameName = new Label(gr.gameName, Assets.SKIN);
-        	Label limit = new Label("Limit: "+gr.timeLimit, Assets.SKIN);
-        	Label mapSize = new Label(map==null?"null":(map.getXSize()+"x"+map.getYSize()), Assets.SKIN);
-        	Label mapName = new Label(map==null?"null":map.name, Assets.SKIN);
-        	
-        	String status = "";
-        	switch(gr.status){
-        	case GameRequest.STATUS_SEARCH_GAME:
-        		status="Searching for game";
-        		break;
-        	case GameRequest.STATUS_UNKNOWN:
-        		status="Status unknown";
-        		break;
-        	case GameRequest.STATUS_WAIT_OPPONENT:
-        		status="Waiting for opponent";
-        		break;
-        	}
-        	
-        	Button cancel = new Button(new Image(Assets.getTextureRegion("cancel")), Assets.SKIN.getStyle("image-toggle", ButtonStyle.class));
-        	final long nonce = gr.nonce;
-        	cancel.setClickListener(new ClickListener() {
-    	        @Override
-    	        public void click(Actor actor,float x,float y ){
-    	        	Main.getInstance().cancelRequest(nonce);
-    	        }
-    		});
-        	
-        	t.left().pad((int)(2*Main.percentWidth));
-        	t.defaults().left().fillX().expandX();
-        	
-        	Table outer = new Table(Assets.SKIN);
-        	outer.defaults().left().height((int)(4*Main.percentHeight));
-        	
-        	Table inner1 = new Table();
-        	inner1.add(gameName).fillX().expandX();
-        	inner1.add(limit);
-        	
-        	Table inner2 = new Table();
-        	inner2.add(mapName).fillX().expandX();
-        	inner2.add(mapSize);
-        	
-        	outer.add(inner1).expandX().fillX();
-        	outer.row();
-        	outer.add(inner2).expandX().fillX();
-        	outer.row();
-        	outer.add(status);
-        	
-        	t.add(outer).expandX().fillX().padLeft((int)(Main.percentHeight));
-        	t.add(cancel).pad((int)(3+Main.percentWidth)).height((int)(7*Main.percentHeight)).width((int)(7*Main.percentHeight)).bottom().right();
-        	
-        	requests.add(t).fillX().expandX().height((int)(17*Main.percentHeight));
+        for(GameRequest request : Main.getInstance().gameRequests.values()){
         	requests.row();
+        	requests.add(gameRequestToTable(request)).fillX().expandX().height((int)(17*Main.percentHeight));
         }
         
         if(current.getActors().size() > 1){
      		gamesList.add(current).fillX().expandX();
          	gamesList.row();
         }
+        
         if(waiting.getActors().size() > 1){
          	gamesList.add(waiting).fillX().expandX();
          	gamesList.row();
         }
+        
         if(requests.getActors().size() > 1){
         	gamesList.add(requests).fillX().expandX();
         }
-        gamesList.padBottom((int)(Main.percentHeight*5));
         
-        updateTimeleftLabels();
+        gamesList.padBottom((int)(Main.percentHeight*5));
 	}
 	
 	public Popup getMapsPopup() {
@@ -423,15 +264,8 @@ public class MainMenu extends MenuScreen {
 			return null;
 	}
 	
-	/**
-	 * Sets logInButton text and also updates games list.
-	 * @param displayName
-	 */
-	public void setDisplayName(String displayName){	//TODO: Move update elsewhere, remove method.
-		updateGamesList();
-	}
-	
 	// Formats the given time to a HH:MM:SS format, timeSpan is given in milliseconds
+	/*
 	private static String formatTime(long milliseconds){
 		long hours = milliseconds / 3600000l;
 		milliseconds %= 3600000l;
@@ -456,6 +290,138 @@ public class MainMenu extends MenuScreen {
 		
 		return builder.toString();
 	}
+	*/
+	
+	private Table gameInstanceToTable(final GameInstance instance){
+    	   	
+    	Label turn = new Label("Turn " + instance.getTurnNumber(), Assets.SKIN);
+    	Label type = new Label(
+    			String.format("(%s)", instance.isAiGame() ? "AI" : (instance instanceof NetworkGameInstance ? "Network" : "Hotseat")), 
+    			Assets.SKIN);
+
+    	Label map = new Label(String.format("'%s'", instance.state.map.name), Assets.SKIN);
+    	Label mapSize = new Label(String.format("%dx%d", instance.state.map.getXSize(), instance.state.map.getYSize()), Assets.SKIN);
+    	Button cancel = new Button(new Image(Assets.getTextureRegion("cancel")), Assets.SKIN.getStyle("image-toggle", ButtonStyle.class));
+    	
+    	cancel.setClickListener(new ClickListener() {
+	        @Override
+	        public void click(Actor actor,float x,float y ){
+	        	instance.resign();
+	        	updateGamesList();
+	        }
+		});
+    	
+    	String opponent = "vs. ";
+    	if(instance.isAiGame() || instance instanceof NetworkGameInstance)
+        	for(Player p : instance.state.players){
+        		if(p.userID != User.globalUserID){
+        			opponent += p.displayedName;
+        			break;
+        		}
+        	}
+    	else{
+    		opponent = instance.state.players[0].displayedName + " vs. " +instance.state.players[1].displayedName;
+    	}
+    	
+    	Label players = new Label(opponent, Assets.SKIN);
+    	
+    	Table table = new Table(Assets.SKIN);
+    	table.setBackground(Assets.SKIN.getPatch("line-border-thin"));
+    	table.left().pad((int)(2*Main.percentWidth));
+    	table.defaults().left().fillX().expandX();
+    	
+    	Table outer = new Table();
+    	outer.defaults().left().height((int)(4*Main.percentHeight));
+    	outer.add(players);
+    	outer.row();
+    	
+    	Table inner1 = new Table();
+    	inner1.add(map).fillX().expandX();
+    	inner1.add(mapSize);
+    	
+    	Table inner2 = new Table();
+    	inner2.add(type);
+    	inner2.add(turn).expandX();
+    	
+    	outer.add(inner1).expandX().fillX();
+    	outer.row();
+    	outer.add(inner2).expandX().fillX();
+    	
+    	table.add(outer).expandX().fillX().padLeft((int)(Main.percentHeight));
+    	table.add(cancel).pad((int)(3+Main.percentWidth)).height((int)(7*Main.percentHeight)).width((int)(7*Main.percentHeight)).bottom().right();
+    	
+    	table.setClickListener(new ClickListener() {
+	        @Override
+	        public void click(Actor actor, float x, float y){
+	        	instance.showGame(false);
+	        }
+		});
+    	
+    	return table;
+	}
+	
+	private Table gameRequestToTable(GameRequest request){
+		Table table = new Table(Assets.SKIN);
+    	table.setBackground(Assets.SKIN.getPatch("line-border-thin"));
+    	
+    	Map map = request.map;
+    	Label gameName = new Label(request.gameName, Assets.SKIN);
+    	Label limit = new Label("Limit: "+request.timeLimit, Assets.SKIN);
+    	Label mapSize = new Label(map==null?"null":(map.getXSize()+"x"+map.getYSize()), Assets.SKIN);
+    	Label mapName = new Label(map==null?"null":map.name, Assets.SKIN);
+    	
+    	String status = "";
+    	switch(request.status){
+	    	case GameRequest.STATUS_SEARCH_GAME: {
+	    		status = "Searching for game";
+	    		break;
+	    	}
+	    	case GameRequest.STATUS_UNKNOWN: {
+	    		status = "Status unknown";
+	    		break;
+	    	}
+	    	case GameRequest.STATUS_WAIT_OPPONENT: {
+	    		status = "Waiting for opponent";
+	    		break;
+	    	}
+    	}
+    	
+    	Button cancel = new Button(new Image(Assets.getTextureRegion("cancel")), Assets.SKIN.getStyle("image-toggle", ButtonStyle.class));
+    	final long nonce = request.nonce;
+    	cancel.setClickListener(new ClickListener() {
+	        @Override
+	        public void click(Actor actor,float x,float y ){
+	        	Main.getInstance().cancelRequest(nonce);
+	        }
+		});
+    	
+    	table.left().pad((int)(2*Main.percentWidth));
+    	table.defaults().left().fillX().expandX();
+    	
+    	Table outer = new Table(Assets.SKIN);
+    	outer.defaults().left().height((int)(4*Main.percentHeight));
+    	
+    	Table inner1 = new Table();
+    	inner1.add(gameName).fillX().expandX();
+    	inner1.add(limit);
+    	
+    	Table inner2 = new Table();
+    	inner2.add(mapName).fillX().expandX();
+    	inner2.add(mapSize);
+    	
+    	outer.add(inner1).expandX().fillX();
+    	outer.row();
+    	outer.add(inner2).expandX().fillX();
+    	outer.row();
+    	outer.add(status);
+    	
+    	table.add(outer).expandX().fillX().padLeft((int)(Main.percentHeight));
+    	table.add(cancel).pad((int)(3+Main.percentWidth)).height((int)(7*Main.percentHeight)).width((int)(7*Main.percentHeight)).bottom().right();
+    	
+    	return table;
+	}
+	
+	
 }
 
 
