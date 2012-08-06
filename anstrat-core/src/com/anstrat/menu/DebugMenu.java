4b5b342b2d6bb340e27091cad5e7b876601ac3af
aabbqrstp <aabbqrstp@92ae5604-1559-cc0f-a502-5bd1a2f9eb42>
2012-08-06 13:44:00 +0000

    Some menu code cleanup + layout fix.

--- anstrat-core/src/com/anstrat/menu/DebugMenu.java
+++ anstrat-core/src/com/anstrat/menu/DebugMenu.java
@@ -1,13 +1,12 @@
 package com.anstrat.menu;
 
+import com.anstrat.core.Assets;
 import com.anstrat.core.Main;
 import com.anstrat.core.Options;
 import com.anstrat.guiComponent.ComponentFactory;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.scenes.scene2d.Actor;
-import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.ui.ClickListener;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.tablelayout.Table;
 
@@ -15,16 +14,18 @@ public class DebugMenu extends MenuScreen {
 
 	private DebugMenu() {
 		super();
-		
-		Button fps = ComponentFactory.createMenuButton("Show FPS: "+(Options.showFps?"ON":"OFF"), new ClickListener() {
+
+        contents.padTop((int) (3*Main.percentHeight));
+        contents.defaults().space((int) (2*Main.percentWidth)).pad(0).top().width(BUTTON_WIDTH).height(BUTTON_HEIGHT);
+        contents.add(ComponentFactory.createMenuButton("Show FPS: "+(Options.showFps?"ON":"OFF"), new ClickListener() {
 			@Override
 			public void click(Actor actor, float x, float y) {
 				Options.showFps = !Options.showFps;
 				((TextButton)actor).setText("Show FPS: "+(Options.showFps?"ON":"OFF"));
 			}
-		});
-		
-		Button resetLogin = ComponentFactory.createMenuButton("Reset login", new ClickListener(){
+		}));
+        contents.row();
+        contents.add(ComponentFactory.createMenuButton("Reset login", new ClickListener(){
             @Override
             public void click(Actor actor, float x, float y){
             	//Main.getInstance().network.logout();
@@ -33,22 +34,12 @@ public class DebugMenu extends MenuScreen {
         		Main.getInstance().network.resetLogin();
         		Main.getInstance().setScreen(AccountMenu.getInstance());
             }
-        });
-        
-		Label loginName = ComponentFactory.createLoginLabel();
-        
-        contents.padTop((int) (3*Main.percentHeight));
-        contents.defaults().space((int) (2*Main.percentWidth)).pad(0).top().width(BUTTON_WIDTH).height(BUTTON_HEIGHT);
-        contents.add(fps);
-        contents.row();
-        contents.add(resetLogin).expandY();
+        })).fillY().expandY();
         contents.row();
         
-        Table inner = new Table();
-        inner.defaults().center();
-        inner.add(loginName);
-        
-        contents.add(inner);
+        Table centerLogin = new Table(Assets.SKIN);
+		centerLogin.add(ComponentFactory.createLoginLabel());
+		contents.add(centerLogin);
 	}
 	
 	private static MenuScreen instance;
