010de11d0d3cfc054964369f55f59d2d4a170d26
EkisSuper <EkisSuper@gmail.com>
2012-08-08 15:46:00 +0000

    test

--- anstrat-core/src/com/anstrat/gameCore/playerAbilities/Zombify.java
+++ anstrat-core/src/com/anstrat/gameCore/playerAbilities/Zombify.java
@@ -1,12 +1,17 @@
 package com.anstrat.gameCore.playerAbilities;
 
+import java.util.List;
+
 import com.anstrat.animation.Animation;
 import com.anstrat.animation.ZombifyAnimation;
+import com.anstrat.core.GameInstance;
+import com.anstrat.gameCore.Fog;
 import com.anstrat.gameCore.Player;
 import com.anstrat.gameCore.State;
 import com.anstrat.gameCore.StateUtils;
 import com.anstrat.gameCore.Unit;
 import com.anstrat.gameCore.UnitType;
+import com.anstrat.geography.Tile;
 import com.anstrat.geography.TileCoordinate;
 import com.anstrat.gui.GEngine;
 import com.badlogic.gdx.Gdx;
@@ -29,6 +34,15 @@ public class Zombify extends TargetedPlayerAbility {
 		Unit zombie = new Unit(UnitType.FALLEN_WARRIOR, player.playerId);
 		zombie.tileCoordinate = tile;
 		State.activeState.addUnit(tile, zombie);
+		List<Tile> tiles = State.activeState.map.getNeighbors(tile);
+		if (State.activeState.map.getTile(tile).visible[GameInstance.activeGame.getUserPlayer().playerId] < 1)
+			State.activeState.map.getTile(tile).visible[GameInstance.activeGame.getUserPlayer().playerId] = 1;
+		for(Tile t : tiles) {
+			if (t.visible[GameInstance.activeGame.getUserPlayer().playerId] < 1) {
+				t.visible[GameInstance.activeGame.getUserPlayer().playerId] = 1;
+			}
+		}
+		Fog.recalculateFog(GameInstance.activeGame.getUserPlayer().playerId, State.activeState);
 		Animation animation = new ZombifyAnimation(target, zombie);
 		GEngine.getInstance().animationHandler.enqueue(animation);
 		State.activeState.unitList.remove(target.id);
