c5e79fa1a70f1208b0164cf8ee01b72a3489a4ee
EkisSuper@gmail.com <EkisSuper@gmail.com@92ae5604-1559-cc0f-a502-5bd1a2f9eb42>
2012-08-14 19:57:36 +0000

    friendpopup including friendlist and the file handling is (should be) workingz

---
+++ trunk/anstrat-core/src/com/anstrat/guiComponent/FriendList.java
@@ -0,0 +1,60 @@
+package com.anstrat.guiComponent;
+
+import java.util.List;
+
+import com.anstrat.core.Assets;
+import com.anstrat.core.Main;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.ui.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.ui.FlickScrollPane;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextField;
+import com.badlogic.gdx.scenes.scene2d.ui.tablelayout.Table;
+
+public class FriendList extends Table {
+	
+	private TextField textField;
+	private Table list;
+	
+	
+	
+	public FriendList(TextField textField){
+		super(Assets.SKIN);
+		this.textField = textField;
+		this.setBackground(Assets.SKIN.getPatch("single-border"));
+	}
+	
+	/**
+	 * Sets the maps of the list.
+	 * @param withRandom Include random map?
+	 * @param filenames F
+	 */
+	public void setFriends(List<String> friends){
+		list = new Table(Assets.SKIN);
+		list.top();
+		
+		//MAPS
+		for(String friend : friends){
+			//Table map = formatMap(mapPath);
+			final TextButton button = new TextButton(friend, Assets.SKIN);
+			if(friend != null){
+				button.setClickListener(new ClickListener() {
+
+					@Override
+					public void click(Actor actor, float x, float y) {
+						textField.setText(button.getText().toString());
+						
+					}
+					
+				});
+				list.row();
+				list.add(button).fillX().expandX().height((int)(10*Main.percentHeight));
+			}
+		}
+		
+		FlickScrollPane scroll = new FlickScrollPane(list);
+		scroll.setScrollingDisabled(true, false);
+		this.add(scroll).fill().expand();
+	}
+	
+}
