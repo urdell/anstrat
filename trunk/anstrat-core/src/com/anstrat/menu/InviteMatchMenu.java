c4b0cb7525c0b15e903f11080b1443ade7dade24
teerik@student.chalmers.se <teerik@student.chalmers.se@92ae5604-1559-cc0f-a502-5bd1a2f9eb42>
2012-08-24 13:17:17 +0000

    edited invite and find match menus

--- trunk/anstrat-core/src/com/anstrat/menu/InviteMatchMenu.java
+++ trunk/anstrat-core/src/com/anstrat/menu/InviteMatchMenu.java
@@ -6,8 +6,6 @@ import com.anstrat.gameCore.playerAbilities.PlayerAbilityType;
 import com.anstrat.guiComponent.ComponentFactory;
 import com.anstrat.popup.InvitePopup;
 import com.anstrat.popup.InvitePopup.InvitePopupHandler;
-import com.anstrat.popup.MapsPopup;
-import com.anstrat.popup.MapsPopup.MapsPopupHandler;
 import com.anstrat.popup.Popup;
 import com.anstrat.popup.TeamPopup;
 import com.anstrat.popup.TeamPopup.TeamPopupListener;
@@ -15,122 +13,45 @@ import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.ui.CheckBox;
 import com.badlogic.gdx.scenes.scene2d.ui.ClickListener;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.tablelayout.Table;
 
 public class InviteMatchMenu extends MenuScreen {
 	private static InviteMatchMenu me;
-	private static TextButton friendButton;
+	private TextButton friendButton;
 	
-	public static int god = PlayerAbilityType.GOD_ODIN, team = TeamPopup.TEAM_VV;
+	private int god = PlayerAbilityType.GOD_ODIN, team = TeamPopup.TEAM_VV;
+	private MapSelecter mapSelecter;
 	
-	private boolean specificMap = false;
-	private boolean generatedMap = false;
-	private boolean randomCustomMap = true;
-	private boolean randomServerdMap = false;
-	
-	private InviteMatchMenu(){
-        
+	private InviteMatchMenu(){        
 		Table settings = new Table(Assets.SKIN);
 		settings.setBackground(Assets.SKIN.getPatch("single-border"));
 		
-		//TextField timeLimit = ComponentFactory.createTextField("Time limit (in minutes)", false);
-		
-		
-		
+		mapSelecter = new MapSelecter();
+
 		CheckBox fog = ComponentFactory.createCheckBox("Fog of War");
 		fog.setChecked(true);
-		
-		final Label mapLabel = new Label(Assets.SKIN);
-		mapLabel.setText("No map chosen");
-		
-		Button mapSpec = ComponentFactory.createButton("Specific", new ClickListener() {
-
-			@Override
-			public void click(Actor actor, float x, float y) {
-				Popup popup = new MapsPopup(new MapsPopupHandler() {
-						@Override
-						public void mapSelected(String map){
-							specificMap = true;
-							generatedMap = false;
-							randomCustomMap = false;
-							randomServerdMap = false;
-							mapLabel.setText(map);
-						}
-					}, false, "Choose specific map", Assets.getMapList(true, true));
-        		
-        		popup.show();
 				
-			}
-			
-		});
-		Button mapCustomRandom = ComponentFactory.createButton("Random custom", new ClickListener() {
-
+		Button godButton = ComponentFactory.createButton("God and team", new ClickListener() {
 			@Override
 			public void click(Actor actor, float x, float y) {
-				
-				specificMap = false;
-				generatedMap = false;
-				randomCustomMap = true;
-				randomServerdMap = false;
-				mapLabel.setText("Random custom map");
-			}
-			
-		});
-		Button mapServerRandom = ComponentFactory.createButton("Random server", new ClickListener() {
-
-			@Override
-			public void click(Actor actor, float x, float y) {
-				specificMap = false;
-				generatedMap = false;
-				randomCustomMap = false;
-				randomServerdMap = true;
-				mapLabel.setText("Random server map");
-				
-			}
-			
-		});
-		Button mapGenerate = ComponentFactory.createButton("Generated", new ClickListener() {
-
-			@Override
-			public void click(Actor actor, float x, float y) {
-				specificMap = false;
-				generatedMap = true;
-				randomCustomMap = false;
-				randomServerdMap = false;
-				mapLabel.setText("Generated map");
-			}
-			
-		});
-		
-		Button god = ComponentFactory.createButton("God and team", new ClickListener() {
-
-			@Override
-			public void click(Actor actor, float x, float y) {
-				Popup popup = new TeamPopup(InviteMatchMenu.god, InviteMatchMenu.team, "Select your team and god", new TeamPopupListener() {
+				Popup popup = new TeamPopup(god, team, "Select your team and god", new TeamPopupListener() {
 
 					@Override
 					public void onChosen(int godChosen, int teamChosen) {
-						InviteMatchMenu.god = godChosen;
-						InviteMatchMenu.team = teamChosen;
+						god = godChosen;
+						team = teamChosen;
 					}
-					
 				});
 				
 				popup.show();
 			}
-			
 		});
 		
 		friendButton = ComponentFactory.createButton("No friend :(", new ClickListener() {
-
 			@Override
 			public void click(Actor actor, float x, float y) {
-				
-				
 				Popup popup = new InvitePopup(new InvitePopupHandler() {
-
 					@Override
 					public void friendSelected(String friend) {
 						// TODO something with this friend of ours.
@@ -140,67 +61,61 @@ public class InviteMatchMenu extends MenuScreen {
 					}
 					
 				}, "Select or write friend's username");
-				popup.show();
-				
+				popup.show();	
 			}
-			
 		});
 		
-		Table mapTable1 = new Table();
-		mapTable1.add(mapSpec).size((int)(Main.percentWidth*37), (int)(Main.percentHeight*10));
-		mapTable1.add(mapServerRandom).size((int)(Main.percentWidth*37), (int)(Main.percentHeight*10));
-		
-		Table mapTable2 = new Table();
-		mapTable2.add(mapGenerate).size((int)(Main.percentWidth*37), (int)(Main.percentHeight*10));
-		mapTable2.add(mapCustomRandom).size((int)(Main.percentWidth*37), (int)(Main.percentHeight*10));
-		
-		settings.defaults().height((int)(Main.percentHeight*10));
-		settings.add("Find Match");
+		settings.add("Invite friend");
 		settings.row();
-		settings.add(mapTable1);
+		settings.add(new MapSelecter()).fillX().expandX();
 		settings.row();
-		settings.add(mapTable2);
-		settings.row();
-		settings.add(mapLabel).center();
-		settings.row();
-		settings.add(friendButton).fillX().expandX();
+		settings.add(friendButton).height(BUTTON_HEIGHT).fillX().expandX();
 		settings.row();
 		settings.add(fog).fillX().expandX();
 		settings.row();
-		settings.add(god).fillX().expandX();
+		settings.add(godButton).height(BUTTON_HEIGHT).fillX().expandX();
 		        
 		TextButton goButton = ComponentFactory.createMenuButton( "GO!",new ClickListener() {
 			@Override
 		    public void click(Actor actor,float x,float y ){
 				
-				if (generatedMap) {
-					Main.getInstance().games.createHotseatGame(null, HotseatMenu.player1god, HotseatMenu.player1team, HotseatMenu.player2god, HotseatMenu.player2team).showGame(true);
-				}
-				else if (randomServerdMap) {
-					String[] maps = Assets.getMapList(false, true);
-					Main.getInstance().games.createHotseatGame(Assets.loadMap(HotseatMenu.getRandom(maps)), HotseatMenu.player1god, HotseatMenu.player1team, HotseatMenu.player2god, HotseatMenu.player2team).showGame(true);
-				}
-				else if (randomCustomMap) {
-					String[] maps = Assets.getMapList(true, true);
-					Main.getInstance().games.createHotseatGame(Assets.loadMap(HotseatMenu.getRandom(maps)), HotseatMenu.player1god, HotseatMenu.player1team, HotseatMenu.player2god, HotseatMenu.player2team).showGame(true);
-				}
-				else if (specificMap) { //specific map
-					String mapName = mapLabel.getText().toString();
-					
-					Main.getInstance().games.createHotseatGame(Assets.loadMap(mapName), HotseatMenu.player1god, HotseatMenu.player1team, HotseatMenu.player2god, HotseatMenu.player2team).showGame(true);
+				int mapChoice = mapSelecter.getMapSelection();
+				
+				switch(mapSelecter.getMapSelection()){
+					case MapSelecter.GENERATED_MAP: {
+						//Main.getInstance().games.createHotseatGame(null, HotseatMenu.player1god, HotseatMenu.player1team, HotseatMenu.player2god, HotseatMenu.player2team).showGame(true);
+						break;
+					}
+					case MapSelecter.RANDOM_MAP: {
+						String[] maps = Assets.getMapList(false, true);
+						//Main.getInstance().games.createHotseatGame(Assets.loadMap(HotseatMenu.getRandom(maps)), HotseatMenu.player1god, HotseatMenu.player1team, HotseatMenu.player2god, HotseatMenu.player2team).showGame(true);
+						break;
+					}
+					case MapSelecter.SPECIFIC_MAP: {
+						String mapName = mapSelecter.getMapName();
+						
+						//Main.getInstance().games.createHotseatGame(Assets.loadMap(mapName), HotseatMenu.player1god, HotseatMenu.player1team, HotseatMenu.player2god, HotseatMenu.player2team).showGame(true);
+						break;
+					}
+					default: {
+						// No map set, potentially show error message
+						break;
+					}
 				}
 		   }
-		} );
-		
+		});
 		
 		contents.padTop((int) (3*Main.percentHeight)).center();
-		contents.defaults().space((int)Main.percentWidth).pad(0).top().width(BUTTON_WIDTH);
+		contents.defaults().center().space((int)Main.percentWidth).pad(0).top().width(BUTTON_WIDTH);
 		contents.add(settings);
 		contents.row();
 		contents.add(goButton).height(BUTTON_HEIGHT).width(BUTTON_WIDTH).padBottom((int) (BUTTON_HEIGHT*1.3));
 		contents.row();
 		Table centerLogin = new Table(Assets.SKIN);
 		centerLogin.add(ComponentFactory.createLoginLabel());
+		contents.row();
+		contents.add().expandY();
+		contents.row();
 		contents.add(centerLogin);
 	}
 	
@@ -218,7 +133,7 @@ public class InviteMatchMenu extends MenuScreen {
 		me = null;
 	}
 	
-	public static void setFriend(String text) {
+	public void setFriend(String text) {
 		friendButton.setText(text);
 	}
 }
