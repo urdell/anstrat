61c14f7708f96a1b289a7e3194a1f32cec3d22dc
aabbqrstp <aabbqrstp@92ae5604-1559-cc0f-a502-5bd1a2f9eb42>
2012-09-20 13:30:03 +0000

    Made player colors a tad darker.

--- trunk/anstrat-core/src/com/anstrat/gameCore/Player.java
+++ trunk/anstrat-core/src/com/anstrat/gameCore/Player.java
@@ -26,7 +26,7 @@ public class Player implements Serializable {
 	private transient IArtificialIntelligence ai = null;
 	
 	// TODO: Proper colors for players 3-4 as well, this is only for 2 players
-	public static final Color[] primaryColor = {new Color(0.2f, 0.2f, 1f, 1f), new Color(1f, 0.15f, 0.15f, 1f), 
+	public static final Color[] primaryColor = {new Color(0f, 0f, 0.6f, 1f), new Color(0.6f, 0f, 0f, 1f), 
 												new Color(0f, 0f, 0f, 0f), new Color(0f, 0f, 0f, 0f)};
 	public static final Color[] secondaryColor = {new Color(0f, 0f, 0.3f, 1f), new Color(0.3f, 0f, 0f, 1f),
 													new Color(0f, 0f, 0f, 0f), new Color(0f, 0f, 0f, 0f)};
