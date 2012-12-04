34dc79e1e58fa18f2fdce707a1df5127571c9630
EkisSuper <EkisSuper@gmail.com>
2012-12-04 22:35:48 +0000

    leap attack fixed

---
+++ anstrat-core/src/com/anstrat/animation/LeapAttackAnimation.java
@@ -0,0 +1,100 @@
+package com.anstrat.animation;
+
+import com.anstrat.core.Assets;
+import com.anstrat.core.GameInstance;
+import com.anstrat.gameCore.Fog;
+import com.anstrat.gameCore.Unit;
+import com.anstrat.geography.TileCoordinate;
+import com.anstrat.gui.GEngine;
+import com.anstrat.gui.GUnit;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.math.Vector2;
+
+public class LeapAttackAnimation extends Animation {
+	private Vector2 start, current, end;
+	private GUnit gunit, target;
+	private int damage;
+	
+	private float xoffset, yoffset, amtOffset;
+	private boolean enterFinished, finishStarted, started;
+	private TileCoordinate startTile, endTile;
+	
+	public LeapAttackAnimation(Unit unit, Unit target, int damage, TileCoordinate startTile, TileCoordinate endTile){
+		GEngine engine = GEngine.getInstance();
+		
+		this.startTile = startTile;
+		this.endTile = endTile;
+		gunit = engine.getUnit(unit);
+		this.target = engine.getUnit(target);
+		this.damage = damage;
+		
+		start = engine.getMap().getTile(startTile).getCenter();
+		end = engine.getMap().getTile(endTile).getCenter();
+		current = new Vector2();
+		Vector2 distance = end.cpy().sub(start);
+		
+		xoffset = distance.x;
+		yoffset = distance.y;
+		
+		length = 1.5f;
+		lifetimeLeft = length;
+		enterFinished = false;
+		finishStarted = false;
+	}
+	
+
+	@Override
+	public void run(float deltaTime) {
+		
+		// Run once
+		if(!started){
+			started = true;
+			gunit.updateHealthbar();
+			gunit.setFacingRight(xoffset >= 0);
+			GEngine.getInstance().updateUI();
+			moveCamera();
+			gunit.playCustom(Assets.getAnimation("berserker-ability-enter"), true);
+			
+		}
+		if(lifetimeLeft <= 1.0f && !enterFinished) {
+			enterFinished = true;
+			System.out.println("hej första");
+			gunit.playCustom(Assets.getAnimation("berserker-ability"), true);
+			target.updateHealthbar();
+			FloatingTextAnimation animation2 = new FloatingTextAnimation(target.unit.tileCoordinate, ""+damage, Color.RED);
+			GEngine.getInstance().animationHandler.runParalell(animation2);
+		}
+		else if(lifetimeLeft <= 0.5f && !finishStarted) {
+			finishStarted = true;
+			System.out.println("hej andra");
+			gunit.playCustom(Assets.getAnimation("berserker-ability-finish"), true);
+		}
+		
+		if(lifetimeLeft <= 0){
+			gunit.setRotation(0);
+			gunit.setPosition(end);
+			gunit.playIdle();	
+		}
+		
+		else if (lifetimeLeft <= 1.0f && lifetimeLeft >= 0.5f){
+			amtOffset = (0.5f-(lifetimeLeft-0.5f))/0.5f;
+			current.set(start.x + xoffset*amtOffset, start.y + yoffset*amtOffset);
+			gunit.setPosition(current);
+		}
+	}
+	
+	private void moveCamera() {
+		if(isVisible()) {
+			Animation animation = new MoveCameraAnimation(end);
+			GEngine.getInstance().animationHandler.runParalell(animation);
+		}
+	}
+
+	@Override
+	public boolean isVisible() {
+		// TODO Auto-generated method stub
+		return Fog.isVisible(startTile,  GameInstance.activeGame.getUserPlayer().playerId) ||
+				Fog.isVisible(endTile,  GameInstance.activeGame.getUserPlayer().playerId);
+		
+	}
+}
