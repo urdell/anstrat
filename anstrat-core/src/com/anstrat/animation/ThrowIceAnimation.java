88a96c864f7815cc8d03738469d28793a1023176
johnny.vayrynen <johnny.vayrynen@gmail.com>
2013-04-04 20:03:13 +0000

    Improved ThrowIce animation.

--- anstrat-core/src/com/anstrat/animation/ThrowIceAnimation.java
+++ anstrat-core/src/com/anstrat/animation/ThrowIceAnimation.java
@@ -1,16 +1,21 @@
 package com.anstrat.animation;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+import java.util.Random;
 import java.util.Set;
 
 import com.anstrat.core.Assets;
+import com.anstrat.gameCore.StateUtils;
 import com.anstrat.gameCore.Unit;
-import com.anstrat.geography.TerrainType;
+import com.anstrat.geography.Tile;
 import com.anstrat.geography.TileCoordinate;
 import com.anstrat.gui.GEngine;
-import com.anstrat.gui.GMap;
 import com.anstrat.gui.GTile;
+import com.anstrat.gui.GUnit;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
@@ -18,36 +23,75 @@ import com.badlogic.gdx.math.Vector2;
 public class ThrowIceAnimation extends Animation {
 
 	private GTile target;
-	private boolean started = false, started2 = false;
+	private GUnit gsource;
+	private Unit source;
+	private boolean started = false, started2 = false, 
+			initialImpact = false, shardImpact = false;
 	private float timePassed = 0;
 	private Vector2 end = null;
-	private float xdiff = 0;
-	private float ydiff = 0;
-	com.badlogic.gdx.graphics.g2d.Animation animation1 = null;
-	com.badlogic.gdx.graphics.g2d.Animation animation2 = null;
+	private List<GTile> splashTiles = null;
+	private float xoffset = 0;
+	private float yoffset = 0;
+	private Vector2 startPos, currPos, targetPos;
+	com.badlogic.gdx.graphics.g2d.Animation j_ability = null;
+	com.badlogic.gdx.graphics.g2d.Animation j_ability_effect = null;
+	com.badlogic.gdx.graphics.g2d.Animation shard_effect = null;
+	private float preparation_time = 0.85f, shard_time = 1.25f;
+	private Sprite shard_sprite = null;
+	private int NUM_SHARDS = 24;
+	private float[] shard_times = new float[NUM_SHARDS];
+	private float[] shard_sizes = new float[NUM_SHARDS];
+	private float[] shard_rotation = new float[NUM_SHARDS];
+	private float[] shard_speed = new float[NUM_SHARDS];
+	Random rand = new Random();
 	
 	Animation shake1 = null, shake2= null, move = null;
 	private Map<Unit, Integer> units;
 	
-	public ThrowIceAnimation(TileCoordinate target, Map<Unit, Integer> units) {
-		animation1 = Assets.getAnimation("meteorstrike");
-		animation2 = Assets.getAnimation("meteorimpact");
+	public ThrowIceAnimation(Unit source, TileCoordinate target, Map<Unit, Integer> units) {		
+		GEngine engine = GEngine.getInstance();
+		
+		for(int i=0;i<NUM_SHARDS;i++){
+			shard_times[i] = rand.nextFloat()/5f;
+			shard_sizes[i] = (rand.nextFloat()+0.5f)/2f;
+			shard_rotation[i] = rand.nextFloat()*6f;
+			shard_speed[i] = (rand.nextFloat()*1.2f + 0.3f) * 2f;
+		}
+		
+		j_ability = Assets.getAnimation("jotun-ability");
+		j_ability_effect = Assets.getAnimation("jotun-ability-effect");
+		shard_effect = Assets.getAnimation("jotun-ability-effect-2");
+				
+		startPos = engine.map.getTile(source.tileCoordinate).getCenter();
+		targetPos = engine.map.getTile(target).getCenter();
+		
+		splashTiles = new ArrayList<GTile>();
+		
+		for(Tile tile : engine.map.map.getNeighbors(target)){
+			splashTiles.add(engine.map.getTile(tile.coordinates));
+		}
+		
+		xoffset = targetPos.x - startPos.x;
+		yoffset = targetPos.y - startPos.y;
 		
 		this.units = units;
 				
-		this.target = GEngine.getInstance().getMap().getTile(target);
-		length = animation1.animationDuration*4+animation2.animationDuration;
+		this.target = engine.getMap().getTile(target);
+		length = preparation_time + j_ability_effect.animationDuration + shard_time + 0.75f;
 		lifetimeLeft = length;
 		
+		gsource = engine.getUnit(source);
+		gsource.setFacingRight(this.target.getPosition().x >= this.gsource.getPosition().x);
 		
 		end = this.target.getPosition();
 		
+		gsource.playCustom(j_ability, false);
 		
-		xdiff = 3*GEngine.getInstance().map.TILE_WIDTH;
-		ydiff = 3*GEngine.getInstance().map.TILE_HEIGHT;
+		//xdiff = 3*GEngine.getInstance().map.TILE_WIDTH;
+		//ydiff = 3*GEngine.getInstance().map.TILE_HEIGHT;
 		
-		shake1 = new ShakeCamAnimation(new Vector2(GEngine.getInstance().camera.position.x, GEngine.getInstance().camera.position.y), GEngine.getInstance().map.TILE_WIDTH/25, GEngine.getInstance().map.TILE_HEIGHT/25, length/8, 12);
-		shake2 = new ShakeCamAnimation(new Vector2(GEngine.getInstance().camera.position.x, GEngine.getInstance().camera.position.y), GEngine.getInstance().map.TILE_WIDTH/15, GEngine.getInstance().map.TILE_HEIGHT/15, 1.1f*animation2.animationDuration, 20);
+		//shake1 = new ShakeCamAnimation(new Vector2(GEngine.getInstance().camera.position.x, GEngine.getInstance().camera.position.y), GEngine.getInstance().map.TILE_WIDTH/25, GEngine.getInstance().map.TILE_HEIGHT/25, length/8, 12);
+		//shake2 = new ShakeCamAnimation(new Vector2(GEngine.getInstance().camera.position.x, GEngine.getInstance().camera.position.y), GEngine.getInstance().map.TILE_WIDTH/15, GEngine.getInstance().map.TILE_HEIGHT/15, 1.1f*animation2.animationDuration, 20);
 		
 	}
 	
@@ -56,15 +100,32 @@ public class ThrowIceAnimation extends Animation {
 		// Run once
 		if(!started){
 			started = true;	
-			GEngine.getInstance().animationHandler.runParalell(shake1);
+			//GEngine.getInstance().animationHandler.runParalell(shake1);
 		}
 		
-		if(lifetimeLeft <= 0f){
+		if(!initialImpact && lifetimeLeft - shard_time - 0.75f <= 0f){
+			initialImpact = true;
+			Unit unit = StateUtils.getUnitByTile(target.tile.coordinates);
+			if(unit != null){
+				unit.resolveDeath();
+				if(unit.currentHP <= 0){
+					GEngine.getInstance().animationHandler.runParalell(new DeathAnimation(unit,
+							GEngine.getInstance().getUnit(unit).isFacingRight()?new Vector2(-1f,0f):new Vector2(1f,0f)));
+				}
+				FloatingTextAnimation animation = new FloatingTextAnimation(unit.tileCoordinate, String.valueOf(units.get(unit)), Color.RED);
+				GEngine.getInstance().animationHandler.runParalell(animation);
+				GEngine.getInstance().getUnit(unit).updateHealthbar();
+				units.remove(unit);
+			}
+		}
+		
+		if(!shardImpact && lifetimeLeft - shard_time / 1.64f - 0.75f <= 0f){
+			shardImpact = true;
 			Set<Unit> list = units.keySet();
 			for (Unit unit: list) {
 				if (unit != null) {
 					if(unit.currentHP <= 0){
-						GEngine.getInstance().animationHandler.enqueue(new DeathAnimation(unit,
+						GEngine.getInstance().animationHandler.runParalell(new DeathAnimation(unit,
 								GEngine.getInstance().getUnit(unit).isFacingRight()?new Vector2(-1f,0f):new Vector2(1f,0f)));
 					}
 					FloatingTextAnimation animation = new FloatingTextAnimation(unit.tileCoordinate, String.valueOf(units.get(unit)), Color.RED);
@@ -73,10 +134,10 @@ public class ThrowIceAnimation extends Animation {
 				}
 			}
 		}
-		if (lifetimeLeft <= animation2.animationDuration && started2 == false) {
-			started2 = true;
-			GEngine.getInstance().animationHandler.runParalell(shake2);
-		}
+		//if (lifetimeLeft <= animation2.animationDuration && started2 == false) {
+		//	started2 = true;
+		//	//GEngine.getInstance().animationHandler.runParalell(shake2);
+		//}
 		timePassed += deltaTime;
 	}
 	
@@ -84,30 +145,50 @@ public class ThrowIceAnimation extends Animation {
 	public void draw(float deltaTime, SpriteBatch batch){
 		super.draw(deltaTime, batch);
 		
-		float var = (lifetimeLeft-animation2.animationDuration)/(length-animation2.animationDuration);
-		
-		if (var < 0)
-			var = 0;
-		
-		GMap map = GEngine.getInstance().map;
-		Vector2 position = new Vector2();
-		position.x = end.x+xdiff*(float)Math.pow(var+0.2, 3); 
-		position.y = end.y-ydiff*(float)Math.pow(var+0.2, 3);
-		
-		float width = map.TILE_WIDTH;
-		float height = map.TILE_HEIGHT;
-		
-		float cw = 4*width*(0.75f+(10*(float)Math.pow(var, 4)));
-		float ch = 4*height*(0.75f+(10*(float)Math.pow(var, 4)));
-		
-		if (lifetimeLeft > animation2.animationDuration){
-			TextureRegion region = this.animation1.getKeyFrame(timePassed, true);
-			batch.draw(region, position.x-cw/2+width/2, position.y-ch/2+height/2, cw, ch);
+		if(timePassed > preparation_time && timePassed < preparation_time + j_ability_effect.animationDuration) {			
+			TextureRegion srcregion = j_ability_effect.getKeyFrame(timePassed-preparation_time, false);
+			
+			float amtOffset = (timePassed-preparation_time) / j_ability_effect.animationDuration;
+			
+			float baseScale = 0.75f;
+			float calcScale = 2f-2f*(float)Math.pow((amtOffset*2f-1),2);
+			float iceScale = baseScale*calcScale;
+			
+			// TODO should not be here, ekis' fault
+			batch.setColor(Color.WHITE);
+			batch.draw(srcregion, startPos.x-srcregion.getRegionWidth() * iceScale / 2 + xoffset*amtOffset, 
+					startPos.y-srcregion.getRegionHeight() *iceScale / 2 + yoffset*amtOffset, 
+					srcregion.getRegionWidth()*iceScale, srcregion.getRegionHeight()*iceScale);
+			
+			//batch.draw(srcregion, startPos.x-srcregion.getRegionWidth() / 2 + xoffset*amtOffset, 
+			//		startPos.y-srcregion.getRegionHeight() / 2 + yoffset*amtOffset);
 		}
-		else {
-			TextureRegion region = this.animation2.getKeyFrame(timePassed-animation1.animationDuration*4, false);
-			batch.draw(region, position.x-(width/2), position.y-(width/2), width*2, height*2);
-		
+		if(timePassed > preparation_time + j_ability_effect.animationDuration){
+			Vector2 offsets = new Vector2();
+			batch.setColor(Color.WHITE);
+			//for(GTile splashing : splashTiles){
+			for(int i=0;i<NUM_SHARDS;i++){
+				if(timePassed > preparation_time + j_ability_effect.animationDuration + shard_times[i]){
+					float angle = (float) i * 360f/24f;
+					float amtOffset = (timePassed - preparation_time - j_ability_effect.animationDuration
+							- shard_times[i])/shard_time;
+					TextureRegion srcregion = shard_effect.getKeyFrame(timePassed-preparation_time, true);
+					offsets.x = (float)Math.cos(Math.toRadians(angle)) * amtOffset * 
+							GEngine.getInstance().map.TILE_SIDE_LENGTH * shard_speed[i];
+					offsets.y = (float)Math.sin(Math.toRadians(angle)) * amtOffset * 
+							GEngine.getInstance().map.TILE_SIDE_LENGTH * shard_speed[i];
+					if(shard_sprite==null)
+						shard_sprite = new Sprite(srcregion);
+					shard_sprite.setRotation(angle+90f+360f*shard_rotation[i]*amtOffset);
+					shard_sprite.setScale(shard_sizes[i]);
+					shard_sprite.setPosition(target.getCenter().x - srcregion.getRegionWidth() / 2f + offsets.x, 
+							target.getCenter().y - srcregion.getRegionHeight() / 2f + offsets.y);
+					Color temp = shard_sprite.getColor();
+					shard_sprite.setColor(temp.r, temp.g, temp.b, amtOffset>=1f?0:(float)Math.sqrt(Math.sqrt(1f - amtOffset)));
+					shard_sprite.draw(batch);
+					shard_sprite.setColor(temp);
+				}
+			}
 		}
 	}
 
