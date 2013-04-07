6b101bdbadc14ae2c77921197e8b42a35be79281
teerik <teerik@student.chalmers.se>
2013-04-07 23:14:03 +0000

    fix invite match menu

--- anstrat-core/src/com/anstrat/menu/InviteMatchMenu.java
+++ anstrat-core/src/com/anstrat/menu/InviteMatchMenu.java
@@ -2,67 +2,69 @@ package com.anstrat.menu;
 
 import com.anstrat.core.Assets;
 import com.anstrat.core.Main;
-import com.anstrat.gameCore.playerAbilities.PlayerAbilityType;
+import com.anstrat.geography.Map;
 import com.anstrat.guiComponent.ComponentFactory;
+import com.anstrat.menu.MapSelecter.MapSelectionHandler;
 import com.anstrat.network.protocol.GameOptions;
+import com.anstrat.network.protocol.GameOptions.MapType;
 import com.anstrat.popup.InvitePopup;
 import com.anstrat.popup.InvitePopup.InvitePopupHandler;
 import com.anstrat.popup.Popup;
-import com.anstrat.popup.TeamPopup;
-import com.anstrat.popup.TeamPopup.TeamPopupListener;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.ui.CheckBox;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
-import com.badlogic.gdx.scenes.scene2d.utils.NinePatchDrawable;
 
-public class InviteMatchMenu extends MenuScreen {
+public class InviteMatchMenu extends MenuScreen implements MapSelectionHandler {
 	private static InviteMatchMenu me;
+	
 	private TextButton friendButton;
 	private String invitedFriend;
 	
-	private int god = PlayerAbilityType.GOD_ODIN, team = TeamPopup.TEAM_VV;
 	private MapSelecter mapSelecter;
+	private PlayerSelecter playerSelecter;
+	private Button goButton;
+	
+	private String mapName;
+	private GameOptions.MapType mapType;
 	
 	private InviteMatchMenu(){      
+		mapSelecter = new MapSelecter(this);
+		playerSelecter = new PlayerSelecter();
 		
-		/*
-		Table settings = new Table(Assets.SKIN);
-		settings.setBackground(new NinePatchDrawable(Assets.SKIN.getPatch("single-border")));
-		
-		mapSelecter = new MapSelecter(true, true);
-
 		final CheckBox fog = ComponentFactory.createCheckBox("Fog of War");
 		fog.setChecked(true);
-				
-		Button godButton = ComponentFactory.createButton("God and team", new ClickListener() {
+		
+		goButton = ComponentFactory.createMenuButton("GO!", new ClickListener() {
 			@Override
 			public void clicked(InputEvent event, float x, float y) {
-				Popup popup = new TeamPopup(team, "Select your team", new TeamPopupListener() {
-
-					@Override
-					public void onChosen(int teamChosen) {
-						team = teamChosen;
-					}
-				});
+				if(goButton.isDisabled()) return;
 				
-				popup.show();
+				// Load named map (if selected)
+				Map map = mapType == GameOptions.MapType.SPECIFIC 
+							      ? Assets.loadMap(mapName)
+							      : null;
+
+				GameOptions options = new GameOptions(map, mapType, playerSelecter.getGod(), playerSelecter.getTeam(), fog.isChecked());
+				
+				Main.getInstance().network.invitePlayer(invitedFriend, options);
 			}
 		});
 		
-		friendButton = ComponentFactory.createButton("No friend :(", new ClickListener() {
+		friendButton = ComponentFactory.createButton("Choose friend", new ClickListener() {
 			@Override
 			public void clicked(InputEvent event, float x, float y) {
 				Popup popup = new InvitePopup(new InvitePopupHandler() {
 					@Override
 					public void friendSelected(String friend) {
-						// TODO something with this friend of ours.
 						Main.getInstance().friends.createFriend(friend);
 						Main.getInstance().friends.saveFriends();
 						friendButton.setText(friend);
 						invitedFriend = friend;
+						
+						checkGoButtonStatus();
 					}
 					
 				}, "Select or write friend's username");
@@ -70,44 +72,23 @@ public class InviteMatchMenu extends MenuScreen {
 			}
 		});
 		
-		settings.add("Invite friend");
-		settings.row();
-		settings.add(mapSelecter).fillX().expandX();
-		settings.row();
-		settings.add(friendButton).height(BUTTON_HEIGHT).fillX().expandX();
-		settings.row();
-		settings.add(fog).fillX().expandX();
-		settings.row();
-		settings.add(godButton).height(BUTTON_HEIGHT).fillX().expandX();
-		        
-		TextButton goButton = ComponentFactory.createMenuButton( "GO!",new ClickListener() {
-			@Override
-			public void clicked(InputEvent event, float x, float y) {
-				
-				Main.getInstance().network.invitePlayer(invitedFriend, new GameOptions(
-						god, 
-						team,
-						fog.isChecked(),
-						mapSelecter.getMapSelection(), 	// TODO doublecheck that this will be the same to all clients and that a correct map can be found using this info.
-						mapSelecter.getMapName(), 
-						null));			// TODO: Set when adding custom maps.
-		   }
-		});
-		
-		contents.padTop((int) (3*Main.percentHeight)).center();
-		contents.defaults().center().space(Main.percentWidth).pad(0).top().width(BUTTON_WIDTH);
-		contents.add(settings);
+		contents.padTop(3f * Main.percentHeight).center();
+		contents.defaults().space(Main.percentWidth).pad(0).top().width(BUTTON_WIDTH);
+		contents.add(mapSelecter);
 		contents.row();
-		contents.add(goButton).height(BUTTON_HEIGHT).width(BUTTON_WIDTH).padBottom(BUTTON_HEIGHT*1.3f);
+		contents.add(playerSelecter);
+		contents.row();
+		contents.add(friendButton).height(BUTTON_HEIGHT);
+		contents.row();
+		contents.add(fog);
+		contents.row();
+		contents.add(goButton).height(BUTTON_HEIGHT).width(BUTTON_WIDTH).padBottom(BUTTON_HEIGHT*0.3f);
 		contents.row();
 		Table centerLogin = new Table(Assets.SKIN);
 		centerLogin.add(ComponentFactory.createLoginLabel());
-		contents.row();
-		contents.add().expandY();
-		contents.row();
-		contents.add(centerLogin);
-		*/
-		contents.add("Redo with same style as hotseat menu");
+		contents.add(centerLogin).bottom();
+		
+		checkGoButtonStatus();
 	}
 	
 	public static synchronized InviteMatchMenu getInstance() {
@@ -124,7 +105,16 @@ public class InviteMatchMenu extends MenuScreen {
 		me = null;
 	}
 	
-	public void setFriend(String text) {
-		friendButton.setText(text);
+	private void checkGoButtonStatus(){
+		this.goButton.setDisabled(invitedFriend == null || this.mapType == null);
+		Assets.SKIN.setEnabled(goButton, !goButton.isDisabled());
+	}
+
+	@Override
+	public void mapSelected(MapType type, String mapName) {
+		this.mapType = type;
+		this.mapName = mapName;
+		
+		checkGoButtonStatus();
 	}
 }
