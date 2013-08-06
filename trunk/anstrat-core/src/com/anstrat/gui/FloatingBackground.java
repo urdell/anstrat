6e62ef6d49c21d8e90990f0117bdb3528a3506bc
EkisSuper@gmail.com <EkisSuper@gmail.com@92ae5604-1559-cc0f-a502-5bd1a2f9eb42>
2013-08-06 14:47:47 +0000

    removed code for painting background. Sry ni som vill ha den :) Also added helpabilititypopupbackground.

--- trunk/anstrat-core/src/com/anstrat/gui/FloatingBackground.java
+++ trunk/anstrat-core/src/com/anstrat/gui/FloatingBackground.java
@@ -17,15 +17,18 @@ public class FloatingBackground {
 	}
 	
 	public void draw(SpriteBatch batch){
-		
+		//background removed atm
+		/*
 		if(background != null){
 			batch.setColor(Color.toFloatBits(0.2f, 0.2f, 0.2f, 1f));
 			//if (State.activeState.map)
-			batch.draw(background, 0.4f*camera.position.x - 500, 0.4f*camera.position.y-500, 1800, 1800);
+			int size = Math.max(State.activeState.map.getXSize(), State.activeState.map.getYSize());
+			double multiplier = size/16;
+			batch.draw(background, 0.4f*camera.position.x - 500, 0.4f*camera.position.y-500, (int)(1800d*multiplier), (int)(1800d*multiplier));
 			//batch.draw(Assets.WHITE, 0.4f*camera.position.x - 500, 0.4f*camera.position.y-500, 1800, 1800);
 			batch.setColor(Color.WHITE);
 		}
-		
+		*/
 	}
 	
 }
