11bbd7b6dff42be067ee81e1a99c932c6d2c40c2
tompa1337@gmail.com <tompa1337@gmail.com@92ae5604-1559-cc0f-a502-5bd1a2f9eb42>
2013-08-18 10:41:52 +0000

    Repacking

--- trunk/anstrat-android/src/com/anstrat/android/GameActivity.java
+++ trunk/anstrat-android/src/com/anstrat/android/GameActivity.java
@@ -2,6 +2,7 @@ package com.anstrat.android;
 
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.anstrat.core.Main;
 import com.badlogic.gdx.backends.android.AndroidApplication;
