7972d78a5eee5fdd6d1337b95db5a568f4adf471
EkisSuper@gmail.com <EkisSuper@gmail.com@92ae5604-1559-cc0f-a502-5bd1a2f9eb42>
2013-08-19 19:38:31 +0000

    Fixed gameinvitepopup, me thinks

--- trunk/anstrat-core/src/com/anstrat/menu/GameInvitePopup.java
+++ trunk/anstrat-core/src/com/anstrat/menu/GameInvitePopup.java
@@ -9,6 +9,7 @@ import com.anstrat.popup.Popup;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.utils.Align;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
 public class GameInvitePopup extends Popup {
@@ -36,9 +37,9 @@ public class GameInvitePopup extends Popup {
 		
 		Label label = new Label(String.format("%s has challenged you to a game!", invite.otherPlayerName), Assets.SKIN);
 
-		add(label);
+		add(label).padBottom(Main.percentHeight*8f).align(Align.center);
 		row();
-		add(team).fillX().expandX().height(Main.percentHeight*8f);
+		add(team).fillX().expandX().height(Main.percentHeight*8f).padBottom(Main.percentHeight*8f);
 		add(new Row(accept, decline));
 	}
 }
